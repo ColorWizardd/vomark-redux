8b1caca125543158e025bde8b86b239b99c41acd
ColorWizardd <jam318@students.uwf.edu>
2026-05-01 15:49:04 -0500

    - Reformed base structure - Added base vocab functionality

---
+++ lib/domain/Graph.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vomark_redux.lib.domain
+{
+    internal abstract class IGraph
+    {
+
+
+    }
+
+    internal class Graph : IGraph
+    {
+
+    }
+
+    internal abstract class ITraversal
+    {
+
+    }
+
+    internal class WeightedRandTraversal : ITraversal
+    {
+
+    }
+
+    internal class GreedyTraversal : ITraversal
+    {
+
+    }
+
+    internal class OptimalTraversal : ITraversal
+    {
+
+    }
+
+}
---
+++ lib/domain/Vocabulary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vomark_redux.lib.domain
+{
+    internal abstract class IVocabulary
+    {
+        protected ConcurrentDictionary<string, int> _vocabList;
+        protected ITokenizer _tokenizer;
+
+        public IVocabulary(ITokenizer tokenizer)
+        {
+            _tokenizer = tokenizer;
+            _vocabList = [];
+        }
+        public IVocabulary(ITokenizer tokenizer, ConcurrentDictionary<string, int> vocabList)
+        {
+            _vocabList = vocabList;
+            _tokenizer = tokenizer;
+        }
+
+        abstract public int GetOrAddToken(string token);
+    }
+
+    internal class Vocabulary : IVocabulary
+    {
+
+        public Vocabulary(ITokenizer tokenizer) : base(tokenizer) { }
+        public Vocabulary(ITokenizer tokenizer, ConcurrentDictionary<string, int> vocabList) : base(tokenizer, vocabList) { }
+        public override int GetOrAddToken(string token)
+        {
+            return _vocabList.GetOrAdd(token, x => _tokenizer.Tokenize(token));
+        }
+    }
+
+    internal interface ITokenizer
+    {
+        abstract int Tokenize(string inp);
+    }
+
+    internal class CountTokenizer : ITokenizer
+    {
+        protected int currCount = 0;
+        public int Tokenize(string inp)
+        {
+            return currCount++;
+        }
+    }
+
+
+}
