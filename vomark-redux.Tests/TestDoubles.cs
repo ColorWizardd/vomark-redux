bac8a382751b5b34ec5fccf4c1ab2c0ef8dd3d01
ColorWizardd <jam318@students.uwf.edu>
2026-05-01 18:29:01 -0500

    Added basic testing for tokenizer.

---
+++ vomark-redux.Tests/TestDoubles.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * Shared collection of test data.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vomark_redux.lib.domain;
+
+namespace vomark_redux.Tests
+{
+    internal class TestDoubles
+    {
+        internal class FakeTokenizer : ITokenizer
+        {
+            int currCount = 0;
+            public int Tokenize(string inp)
+            {
+                return currCount++;
+            }
+        }
+    }
+}
---
+++ vomark-redux.Tests/VocabularyTests/VocabularyTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vomark_redux.lib.domain;
+using Xunit;
+using static vomark_redux.Tests.TestDoubles;
+
+namespace vomark_redux.Tests.VocabularyTests
+{
+    public class VocabularyTest
+    {
+        public class VocabBaseTest
+        {
+            [Fact]
+            public void TestAddToken()
+            {
+                FakeTokenizer ft = new();
+                Vocabulary voc = new(ft);
+
+                voc.GetOrAddToken("test1");
+                voc.GetOrAddToken("test2");
+                Assert.Equal(1, voc.GetOrAddToken("test2"));
+                Assert.Equal(0, voc.GetOrAddToken("test1"));
+                Assert.Equal(2, voc.GetOrAddToken("test3"));
+            }
+        }
+    }
+}
