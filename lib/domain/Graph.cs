6525a630af241fac65946a1700b4b10ab3462716
ColorWizardd <jam318@students.uwf.edu>
2026-05-02 11:43:31 -0500

    Completed base graph behavior

--- lib/domain/Graph.cs
+++ lib/domain/Graph.cs
@@ -11,9 +11,10 @@ namespace vomark_redux.lib.domain
     public abstract class IGraph
     {
         protected IVocabulary? _vocabulary;
-        protected ConcurrentDictionary<(string, string), int> _edgeList;
+        protected ConcurrentDictionary<string, ConcurrentDictionary<string, int>> _edgeList;
         protected ITraversal? _traversal;
         protected ITokenizer? _tokenizer;
+        protected Random? _rand;
         protected string _name;
 
         public IGraph(string name) { _name = name; _edgeList = []; }
@@ -21,10 +22,12 @@ namespace vomark_redux.lib.domain
         public void SetTraversal(ITraversal tra) { _traversal = tra; }
         public void SetName(string name) { _name = name; }
         public void SetTokenizer(ITokenizer tok) { _tokenizer = tok; }
+        public void SetRandom(Random rand) { _rand = rand; }
 
         abstract public void AddOrStrengthenEdge(string from, string to, int weight = 1);
         abstract public int GetWeight(string from, string to);
-        abstract public string GetNextNode(string curr);
+        abstract public string? GetNextNode(string curr);
+        abstract public ConcurrentDictionary<string, int>? FindAllNext(string curr);
     }
 
     public class Graph : IGraph
@@ -33,33 +36,70 @@ namespace vomark_redux.lib.domain
 
         public override void AddOrStrengthenEdge(string from, string to, int weight = 1)
         {
-            _edgeList.AddOrUpdate((from, to), weight, (key, oldWeight) => oldWeight + weight);
+            var nextList = _edgeList.GetOrAdd(from, x => new ConcurrentDictionary<string, int>());
+            nextList.AddOrUpdate(to, weight, (x, oldWeight) => weight + oldWeight);
         }
 
-        public override string GetNextNode(string curr)
+        public override ConcurrentDictionary<string, int>? FindAllNext(string curr)
         {
-            throw new NotImplementedException();
+            if (_edgeList.TryGetValue(curr, out var nextList)){
+                return nextList;
+            }
+            return null;
+        }
+
+        public override string? GetNextNode(string curr)
+        {
+            if(_traversal == null || _rand == null) { return null; }
+            var nextList = FindAllNext(curr);
+            if (nextList != null)
+            {
+            return _traversal.Next(nextList, _rand);
+            }
+            return null;
         }
 
         public override int GetWeight(string from, string to)
         {
-            throw new NotImplementedException();
+            if(_edgeList.TryGetValue(from, out var nextList))
+            {
+                nextList.TryGetValue(to, out int res);
+                return res;
+            }
+            return -1;
         }
     }
 
     public abstract class ITraversal
     {
-
+        internal abstract string? Next(ConcurrentDictionary<string, int> nextList, Random rand);
     }
 
     public class WeightedRandTraversal : ITraversal
     {
-
+        internal override string? Next(ConcurrentDictionary<string, int> nextList, Random rand)
+        {
+            int weightSum = nextList.Values.Sum();
+            int currThresh = 0;
+            int thresh = rand.Next(0, weightSum);
+            foreach(string key in nextList.Keys)
+            {
+                currThresh += nextList[key];
+                if(currThresh >= thresh)
+                {
+                    return key;
+                }
+            }
+            return null;
+        }
     }
 
     public class GreedyTraversal : ITraversal
     {
-
+        internal override string? Next(ConcurrentDictionary<string, int> nextList, Random rand)
+        {
+            return nextList.Max().Key;
+        }
     }
 
 }
